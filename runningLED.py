9565d03dc9df167d8f37aa5b60ab91260115c6fd
Ian Young <ian.young@verkada.com>
2024-01-25 11:38:18 -0700

    Some GPIO shenanigans

---
+++ runningLED.py
@@ -0,0 +1,28 @@
+import time
+import logging
+
+log = logging.getLogger()
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(levelname)s: %(message)s"
+)
+
+try:
+    import RPi.GPIO as GPIO  # type: ignore
+    GPIO.setup(7, GPIO.OUT)
+except ImportError:
+    GPIO = None
+    log.critical("RPi.GPIO is not availbale. Running on a non-Pi platform")
+
+
+def work():
+    log.debug("Running work function")
+    for _ in range(0, 5):
+        time.sleep(1)
+
+
+GPIO.output(7, True)
+log.debug("LED on.")
+work()
+GPIO.output(7, False)
+log.debug("LED off.")
