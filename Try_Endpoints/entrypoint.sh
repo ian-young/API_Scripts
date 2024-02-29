82b2f4ca421cc7501b6fcd641c26ae07f910f6fc
Ian Young <ian.young@verkada.com>
2024-02-29 15:08:04 -0700

    Docker container to graph endpoints on localhost.

--- Try_Endpoints/entrypoint.sh
+++ Try_Endpoints/entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 service cron start
 echo "Gathering data..."
-python3 endpoint_tests.py > /var/log/endpoint_tests.log 2>&1
-echo "--Updating graph with newly acquired data--"
-python3 update_graph.py > /var/log/update_graph.log 2>&1
+python3 endpointTests.py > /var/log/endpointTests.log 2>&1
+python3 updateGraph.py > /var/log/updateGraph.log 2>&1
 echo "Starting webserver"
 python3 webserver.py > /var/log/webserver.log 2>&1 &
 echo "Ready to rock and roll!"
--- Try_Endpoints/webserver.py
+++ Try_Endpoints/webserver.py
@@ -1,9 +1,5 @@
-"""
-Author: Ian Young
-Purpose: Host a Flask webserver to print a graph on localhost.
-"""
 from flask import Flask, app, send_file
-from update_graph import image_path as image
+from updateGraph import image_path as image
 
 # Initialize Flask
 app = Flask(__name__)
@@ -11,9 +7,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    """Sends the file to the webserver to create the webpage."""
     return send_file(image, mimetype='image/png')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)  # Run on local host
+    app.run(host='0.0.0.0', port=5000)
