d8d19a0a9c620f114841d50754f49374f2e969af
Ian Young <ian.young@verkada.com>
2024-07-31 14:19:08 -0600

    Will create a csv of all users that are missing in Command that are found in the sis and legacy system along with their assigned group(s).

--- Customer_Projects/pull_needed_ac_users.py
+++ Customer_Projects/pull_needed_ac_users.py
@@ -1,55 +1,58 @@
 """
 Author: Ian Young
-Purpose: This script will compare two csvs and generate a third with
-access users that should be in Command.
+Purpose: This script will compare three csvs and generate a fourth with
+    access users that are in the old system and sis but not in Command.
 """
 
+import concurrent.futures
 import csv
-import logging
 import gc
+import logging
 from os import getpid
-from typing import List, Dict
+from typing import Dict, List
 
-from QoL.verbose_compute import memory_usage, cpu_usage
+from tqdm import tqdm
+
+from QoL.verbose_compute import memory_usage
 
 PID = getpid()
 
-CSV_OUTPUT = (
-    "/Users/ian.young/Documents/.scripts/API_Scripts/"
-    "Customer_Projects/formatted_users.csv"
-)
-CSV_AC_LIST = (
-    "/Users/ian.young/Documents/.scripts/API_Scripts/"
-    "Customer_Projects/test_csv_1.csv"
-)
-CSV_SIS_USERS = (
-    "/Users/ian.young/Documents/.scripts/API_Scripts/"
-    "Customer_Projects/test_csv_2.csv"
-)
+CSV_OUTPUT = "formatted_users.csv"
+CSV_AC_LIST = "LegacySystemExport.csv"
+CSV_SIS_USERS = "SISExport.csv"
+CSV_CURRENT_USERS = "VerkadaExport.csv"
 CARD_TYPE = "Standard 26-bit Wiegand"
 STATUS = "Active"
+
+SCHOOL_ID_MAPPING = {
+    "704": "(JHS)",
+    "304": "(JJHS)",
+    "114": "(ES)",
+    "110": "(ESS)",
+    "112": "(ESSS)"
+}
 
 calculate_memory = lambda start_mem, end_mem: end_mem - start_mem
 
 log = logging.getLogger()
-LOG_LEVEL = logging.DEBUG
+LOG_LEVEL = logging.WARNING
 logging.basicConfig(
-    level=LOG_LEVEL,
-    format="(%(asctime)s.%(msecs)03d) %(levelname)s: %(message)s",
-    datefmt="%H:%M:%S",
+    level=LOG_LEVEL, format="(%(asctime)s) %(levelname)s: %(message)s"
 )
 log.setLevel(LOG_LEVEL)
 
 
 def read_ac_csv(file_name: str) -> List[Dict[str, str]]:
     """
-    Reads a CSV file and extracts the first name, last name, and email columns.
+    Reads a CSV file and extracts the first name, last name, and email
+        columns.
 
     Args:
         file_path (str): The path to the CSV file.
 
     Returns:
-        list of dict: A list of dictionaries containing 'first_name', 'last_name', and 'email'.
+        list of dict: A list of dictionaries containing 'first_name',
+            'last_name', and 'email'.
     """
     start_mem = memory_usage(PID)
     data: List[Dict[str, str]] = []
@@ -59,18 +62,27 @@ def read_ac_csv(file_name: str) -> List[Dict[str, str]]:
         # Set reader
         csv_reader = csv.DictReader(csv_file)
 
-        log.debug("Parsing access control csv")
-        # Extract useful columns
-        data.extend(
-            {
-                "Email": row["Email"],
-                "Facility Code": row["Facility Code"],
-                "Card Number": row["Card Number"],
-                "Entry Code": row["Entry Code"],
-            }
-            for row in csv_reader
-        )
-    log.debug("Data retrieved")
+        log.info("Parsing access control csv")
+        for row in tqdm(csv_reader, desc="Read legacy file"):
+            # Extract useful columns
+            name_parts = row["Name"].split(", ")
+            if len(name_parts) == 2:
+                data.append(
+                    {
+                        "First Name": name_parts[1],
+                        "Last Name": name_parts[0],
+                        "Card Number": row["cardNumber"],
+                    }
+                )
+            else:
+                data.append(
+                    {
+                        "First Name": name_parts[0],
+                        "Last Name": "",
+                        "Card Number": row["cardNumber"],
+                    }
+                )
+    log.info("Data retrieved")
     log.debug(
         "Total memory used: %iKiB",
         calculate_memory(start_mem, memory_usage(PID)),
@@ -83,13 +95,80 @@ def read_ac_csv(file_name: str) -> List[Dict[str, str]]:
 
 def read_sis_csv(file_name: str) -> List[Dict[str, str]]:
     """
-    Reads a CSV file and extracts the first name, last name, and email columns.
+    Reads a CSV file and extracts the first name, last name, and email
+        columns.
 
     Args:
         file_path (str): The path to the CSV file.
 
     Returns:
-        list of dict: A list of dictionaries containing 'first_name', 'last_name', and 'email'.
+        list of dict: A list of dictionaries containing 'first_name',
+            'last_name', and 'email'.
+    """
+    start_mem = memory_usage(PID)
+    data: List[Dict[str, str]] = []
+    recorded_emails = set()  # Will help avoid duplicate entries
+
+    # Open file
+    with open(file_name, mode="r", newline="", encoding="UTF-8") as csv_file:
+        # Set reader
+        csv_reader = csv.DictReader(csv_file)
+
+        log.info("Parsing sis csv")
+        # Extract useful columns
+        for row in tqdm(csv_reader, desc="Read sis csv"):
+            if (
+                row["Email_Addr"] not in recorded_emails
+                and row["Email_Addr"] != ""
+            ):
+                try:
+                    data.append(
+                        {
+                            "School ID": row["SchoolID"],
+                            "First Name": row["First_Name"],
+                            "Last Name": row["Last_Name"],
+                            "Email": row["Email_Addr"],
+                        }
+                    )
+
+                    recorded_emails.add(row["Email_Addr"])  # Track email
+
+                except IndexError:
+                    data.append(
+                        {
+                            "First Name": "",
+                            "Last Name": "",
+                            "Email": row["Email_Addr"],
+                        }
+                    )
+                    log.error(
+                        "%s: Either first name or last name was provided.",
+                        str(row),
+                    )
+                    continue
+
+    log.info("Data retrieved")
+    log.debug(
+        "Total memory used: %iKiB",
+        calculate_memory(start_mem, memory_usage(PID)),
+    )
+
+    gc.collect()  # Clear out variables from memory
+
+    return data
+
+
+def read_command_csv(file_name: str) -> List[Dict[str, str]]:
+    """
+    Reads a CSV file and extracts the first name, last name, card number
+        and email columns.
+
+    Args:
+        file_path (str): The path to the CSV file.
+
+    Returns:
+        list of dict: A list of dictionaries containing 'first_name',
+            'last_name', and 'email'.
     """
     start_mem = memory_usage(PID)
     data: List[Dict[str, str]] = []
@@ -99,17 +178,16 @@ def read_sis_csv(file_name: str) -> List[Dict[str, str]]:
         # Set reader
         csv_reader = csv.DictReader(csv_file)
 
-        log.debug("Parsing sis csv")
+        log.info("Parsing sis csv")
         # Extract useful columns
-        for row in csv_reader:
+        for row in tqdm(csv_reader, desc="Read Command csv"):
             try:
-                name_parts = row["Name"].split()
                 data.append(
                     {
-                        "First Name": name_parts[0],
-                        "Last Name": name_parts[-1],
-                        "Email": row["Email"],
-                        "External ID": row["Employee ID"],
+                        "First Name": row["firstName"],
+                        "Last Name": row["lastName"],
+                        "Email": row["email"],
+                        "Card Number": row["cardNumber"],
                     }
                 )
 
@@ -127,7 +205,7 @@ def read_sis_csv(file_name: str) -> List[Dict[str, str]]:
                 )
                 continue
 
-    log.debug("Data retrieved")
+    log.info("Data retrieved")
     log.debug(
         "Total memory used: %iKiB",
         calculate_memory(start_mem, memory_usage(PID)),
@@ -138,11 +216,99 @@ def read_sis_csv(file_name: str) -> List[Dict[str, str]]:
     return data
 
 
+def collect_groups(sis_users_list, index, current_email=None, collected_groups=None):
+    """
+    Collect groups associated with a specific email from the SIS users
+    list recursively.
+
+    Args:
+        sis_users_list (list): A list of dictionaries containing user
+            information.
+        index (int): The current index in the SIS users list.
+        current_email (str, optional): The current email being processed.
+            Defaults to None.
+        collected_groups (list, optional): List of collected groups for
+            the current email. Defaults to None.
+
+    Returns:
+        list: A list of groups associated with the specified email.
+    """
+
+    if collected_groups is None:
+        collected_groups = []
+
+    # Base case: End of the list
+    if index >= len(sis_users_list):
+        return collected_groups
+
+    current_user = sis_users_list[index]
+    email = current_user["Email"]
+
+    # Initialize current_email on the first call
+    if current_email is None:
+        current_email = email
+
+    # If we have moved to a different email, return the accumulated groups
+    if email != current_email:
+        return collected_groups
+
+    # Process the current SchoolID
+    school_id = current_user["School ID"]
+
+    if school_id in SCHOOL_ID_MAPPING:
+        group = SCHOOL_ID_MAPPING[school_id]
+
+        if group not in collected_groups:
+            collected_groups.append(group)
+
+    # Recurse to the next item
+    return collect_groups(sis_users_list, index + 1, current_email, collected_groups)
+
+
+def process_sis_users(sis_users_list: List[Dict[str, str]]) -> Dict[str,str]:
+    """
+    Process the SIS users list to collect user groups for each unique
+        email.
+
+    Args:
+        sis_users_list (list): A list of dictionaries containing user
+            information, including email.
+
+    Returns:
+        dict: A dictionary where keys are unique email addresses and
+            values are comma-separated user groups.
+    """
+
+    # Initialize an empty dictionary for storing user groups
+    user_groups = {}
+
+    index = 0
+    while index < len(sis_users_list):
+        current_user = sis_users_list[index]
+
+        if email := current_user["Email"]:
+            if email not in user_groups:
+
+                # Collect groups for the current email
+                groups = collect_groups(sis_users_list, index, email)
+                user_groups[email] = ";".join(groups)
+
+            # Skip ahead to the next unique email
+            while index < len(sis_users_list) and sis_users_list[index]["Email"] == email:
+                index += 1
+
+        else:
+            index += 1
+
+    return user_groups
+
+
 def compile_data_for_csv(
-    third_party_csv: str, sis_csv: str
+    third_party_csv: str, sis_csv: str, current_csv: str
 ) -> List[Dict[str, str]]:
     """
-    Will take two dictionaries and prepare the data to be written to a csv.
+    Will take two dictionaries and prepare the data to be written to a
+        csv.
 
     Args:
         command_csv (list of dict): The imported data from the csv
@@ -153,48 +319,64 @@ def compile_data_for_csv(
     Returns:
         list of dict: Returns all matching values between the two csvs.
     """
-    start_cpu = cpu_usage(PID, None)
     start_mem = memory_usage(PID)
     compiled_data = []
 
     log.info("Reading csv files")
-    ac_users_list = read_ac_csv(third_party_csv)
-    sis_users_list = read_sis_csv(sis_csv)
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future_ac_users = executor.submit(read_ac_csv, third_party_csv)
+        future_sis_users = executor.submit(read_sis_csv, sis_csv)
+        future_current_users = executor.submit(read_command_csv, current_csv)
+
+        ac_users_list = future_ac_users.result()
+        sis_users_list = future_sis_users.result()
+        current_users_list = future_current_users.result()
     log.info("CSV files read successfully")
 
     # Creating a lookup dictionary
-    existing_ac_user_emails = {user["Email"]: user for user in ac_users_list}
+    current_users_lookup = {
+        user["Email"]: user for user in current_users_list if user["Email"]
+    }
+    ac_users_lookup = {
+        (user["First Name"].strip(), user["Last Name"].strip()): user
+        for user in ac_users_list
+    }
 
-    for user in sis_users_list:
-        log.debug("Running for: %s", str(user))
+    # Process SIS users to collect groups
+    user_groups = process_sis_users(sis_users_list)
+
+    for sis_user in sis_users_list:
         try:
-            if email := user["Email"]:
-                log.debug("Testing for %s", user["Email"])
-                if email in existing_ac_user_emails:
+            email = sis_user["Email"]
+            if (
+                email
+                and email in current_users_lookup
+                and not current_users_lookup[email]["Card Number"]
+            ):
+                first_name = sis_user["First Name"]
+                last_name = sis_user["Last Name"]
+
+                log.debug("Checking names %s", first_name)
+
+                if ac_user := ac_users_lookup.get((first_name, last_name)):
+                    log.debug("Match! Adding to compiled data")
                     compiled_data.append(
                         {
-                            "Email": email,
-                            "External ID": user["External ID"],
-                            "Card Type": CARD_TYPE,
-                            "Facility Code": existing_ac_user_emails[email][
-                                "Facility Code"
-                            ],
-                            "Card Number": existing_ac_user_emails[email][
-                                "Card Number"
-                            ],
-                            "Card Number Hex": "",
-                            "License Plate": "",
-                            "Entry Code": existing_ac_user_emails[email][
-                                "Entry Code"
-                            ],
-                            "Credential Status": STATUS,
+                            "firstName": first_name,
+                            "lastName": last_name,
+                            "email": email,
+                            "cardFormat": CARD_TYPE,
+                            "facilityCode": "103",
+                            "cardNumber": ac_user["Card Number"],
+                            "groups": user_groups.get(email, "")
                         }
                     )
+
                 else:
                     log.debug("No matching case for %s", email)
 
         except KeyError as e:
-            log.warning("A field was found empty in %s", user)
+            log.warning("A field was found empty in %s", sis_user)
             log.error(e)
             continue
 
@@ -203,27 +385,46 @@ def compile_data_for_csv(
         "Total memory used: %iKiB",
         calculate_memory(start_mem, memory_usage(PID)),
     )
-    log.debug("CPU utilization: %.1f%%", cpu_usage(PID, None) - start_cpu)
 
     gc.collect()  # Clear out variables from memory
 
     return compiled_data
 
 
-if ac_user_list := compile_data_for_csv(CSV_AC_LIST, CSV_SIS_USERS):
+if ac_user_list := compile_data_for_csv(
+    CSV_AC_LIST, CSV_SIS_USERS, CSV_CURRENT_USERS
+):
+    log.info("Writing file")
     with open(CSV_OUTPUT, "w", newline="", encoding="UTF-8") as file:
         fieldnames = (
-            "Email",
-            "User ID",
-            "External ID",
-            "Card Type",
-            "Facility Code",
-            "Card Number",
-            "Card Number Hex",
-            "License Plate",
-            "Entry Code",
-            "Credential Status",
+            "firstName",
+            "lastName",
+            "middleName",
+            "email",
+            "userId",
+            "externalId",
+            "companyName",
+            "employeeId",
+            "employeeType",
+            "employeeTitle",
+            "department",
+            "departmentId",
+            "startDate",
+            "endDate",
+            "roles",
+            "cardFormat",
+            "facilityCode",
+            "cardNumber",
+            "cardNumberHex",
+            "licensePlateNumbers",
+            "entryCode",
+            "groups",
+            "cloudUnlock",
+            "bluetoothUnlock",
+            "photoUrl"
         )
         writer = csv.DictWriter(file, fieldnames=fieldnames)
         writer.writeheader()
-        writer.writerows(ac_user_list)
+        for final_row in tqdm(ac_user_list, desc="Writing Missing Users"):
+            writer.writerow(final_row)
+    log.info("Written to file")
